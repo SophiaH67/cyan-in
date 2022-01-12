4d8098d8005271613b3d368873b55e04cc0b33d6
marnixah <marnix@hagehoeve.com>
2022-01-13 00:40:50 +0100

    fix(tailwind.config.js): remove unused import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,6 @@
-import colors from 'tailwindcss/colors';
-
 module.exports = {
   content: ['./src/renderer/**/*.{js,jsx,ts,tsx,ejs}'],
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        sky: colors.sky,
-        cyan: colors.cyan,
-      },
-    },
-  },
   variants: {
     extend: {},
   },
